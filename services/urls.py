d62b5afcb6723a92c13f6f82d1e569d80fae09f3
rohit-r7 <reghunathrohit@gmail.com>
2022-10-13 11:13:31 +0530

    First

---
+++ services/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import include, path
+from . import views
+app_name = 'services'
+urlpatterns = [
+   path('',views.index,name='book'),
+   path('food',views.home,name='fud'),
+   path('room',views.rom,name='rom')
+]
---
+++ services/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'bookings.html')
+
+def home(request):
+    return render(request,'food.html')
+
+def rom(request):
+    return render(request,'room.html')
